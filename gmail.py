0e0db341f96a98c457552b5e0a4f48d4bd3859fe
Jeffrey Estrada <jestrada@wpi.edu>
2017-10-28 10:06:04 -0400

    add some stuff

--- gmail.py
+++ gmail.py
@@ -11,3 +11,10 @@ class email():
 	def send(self, to, msg):
 		self.server.sendmail(self.email, to, msg)
 		self.server.quit()
+
+msg_format = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s"
+
+
+an_email = email()
+an_email.send("jestrada@wpi.edu", "Testing this shit out")
+
---
+++ market.sh
@@ -0,0 +1,1 @@
+# Author: Jeffrey Estrada
